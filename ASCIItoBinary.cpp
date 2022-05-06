3b8629d9789f17e2074023dfc053a193f753280b
Giovanni Martinovsky <104166852+Ph4ndor4XxX@users.noreply.github.com>
2022-05-06 18:34:42 -0400

    Update ASCIItoBinary.cpp

--- ASCIItoBinary.cpp
+++ ASCIItoBinary.cpp
@@ -1,76 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int convertBinario(int n){
-   if(n == 0)
-        return 0;
-    else
-    {
-        if(n == -61|| n == -62)
-        {
-        n = 128+n+128;
-        }
-        else if(n<0 && n!=-61 && n!= -62)
-        {
-            n = 256+n;
-        }
-        convertBinario(n/2);
-        return printf("%d", n % 2);
-    }
-}
-int convertBinario2(FILE *file,int n);
-int main(void)
-{
-    FILE *file;
-    file = fopen("arquivo.txt","r");
+#include <conio.h>
 
-    if(file == NULL)
-    {
-        printf("Nao foi possivel abrir o arquivo.\n");
+int convertBinary1(int n);
+int convertBinary2(FILE *file,int n);
+
+int main(void){
+    FILE *file;
+    file = fopen("file.txt","r");
+
+    if(file == NULL){
+        printf("Could not open file.\n");
         exit(0);
     }
-    char texto[1000000];
-    while(fgets(texto,1000000,file)!= NULL)
-    {
-        printf("%s\n",texto);
+    char text[1000000];
+    while(fgets(text,1000000,file)!= NULL){
+        printf("%s\n",text);
     }
 
-    printf("%d%d\n",texto[0],texto[1]);
-    printf("%d\n",strlen(texto));
+    printf("%d%d\n",text[0],text[1]);
+    printf("%d\n",strlen(text));
 
-    int cont = strlen(texto);
+    int cont = strlen(text);
 
-    for(int i =0; i < cont ;i++)
-    {
-        if(texto[i]!=' ')
-            convertBinario(texto[i]);
+    for(int i =0; i < cont ;i++){
+        if(text[i]!=' ')
+            convertBinary1(text[i]);
             printf(" ");
     }
 
     fclose(file);
-    file = fopen("arquivo.txt","a");
+    file = fopen("file.txt","a");
         fprintf(file,"\n");
         for(int i =0; i < cont ;i++){
-            if(texto[i]!=' ')
-                convertBinario2(file,texto[i]);
+            if(text[i]!=' ')
+                convertBinary2(file,text[i]);
         fprintf(file," ");
         }
     fclose(file);
-
 }
-int convertBinario2(FILE *file,int n){
+
+int convertBinary1(int n){
+   if(n == 0)
+        return 0;
+    else{
+        if(n == -61|| n == -62)
+        n = 128+n+128;
+        else if(n<0 && n!=-61 && n!= -62)
+            n = 256+n;
+        convertBinary1(n/2);
+        return printf("%d", n % 2);
+    }
+}
+
+int convertBinary2(FILE *file,int n){
 if(n == 0)
     return 0;
     else{
         if(n == -61|| n == -62)
-        {
         n = 128+n+128;
-        }
         else if(n<0 && n!=-61 && n!= -62)
-        {
             n = 256+n;
-        }
-        convertBinario2(file,n/2);
+        convertBinary2(file,n/2);
         return fprintf(file,"%d",n % 2);
     }
 }
